3a9c3800dc90e9923948bc1e199e0f3a42bad0e5
Parshvavyas17 <parshvavyas07@gmail.com>
2022-08-11 18:53:20 +0530

    Post vacancy page fixes

--- client/src/pages/Company/CompanyPostVacancy.js
+++ client/src/pages/Company/CompanyPostVacancy.js
@@ -1,15 +1,24 @@
-import SideBarCompany from "../../components/SideBarCompany"
+import SideBarCompany from '../../components/SideBarCompany';
+import { useState } from 'react';
 
 const CompanyPostVacancy = () => {
+    const [ jobType, setJobType ] = useState('internship');
+    const toggleJobType = (e) => {
+        if(jobType === 'internship') {
+            setJobType('job');
+        } else {
+            setJobType('internship');
+        }
+    }
   return (
-    <div className='bg-purple w-full h-screen flex font-main'>
+    <div className='bg-purple w-full h-fit flex font-main font-ourfont'>
         <SideBarCompany selected={`Post Vacancy`} />
-        <div className="bg-[#F2F2F2] w-full px-10 pt-4 ml-10 rounded-l-3xl flex flex-wrap justify-center items-center">
-            <div className="flex flex-col w-1/2 h1/2">
+        <div className="container bg-[#F2F2F2] w-full px-10 pt-4 ml-10 rounded-l-3xl flex flex-wrap justify-center items-center">
+            <div className="flex flex-col w-1/2 h1/2 py-8">
                 <div className="flex flex-row items-baseline content-center justify-center">
                     <label className="text-center text-xl font-bold pb-4 mr-3">Vacancy Type:</label>
-                    <select name="type" className="rounded-sm border border-purple w-48 focus:outline-none pl-2 py-1">
-                        <option value="internship" selected>Internship</option>
+                    <select name="type" className="rounded-sm border border-purple w-48 focus:outline-none pl-2 py-1" onChange={toggleJobType}>
+                        <option value="internship">Internship</option>
                         <option value="job">Job</option>
                     </select>
                 </div>
@@ -35,7 +44,7 @@ const CompanyPostVacancy = () => {
                                 <input type='text' name='title' className="rounded-xl border border-purple w-48 focus:outline-none pl-4 py-1" />
                             </div>
                             <div className="flex flex-col min-w-[40%] min-h-full mb-4">
-                                <label className="mb-4 font-bold">Duration:</label>
+                                <label className="mb-4 font-bold">{(jobType === 'internship') ? 'Duration:' : 'Salary:'}</label>
                                 <input type='text' name='positionCount' className="rounded-xl border border-purple w-48 focus:outline-none pl-4 py-1" />
                             </div>
                         </div>
@@ -49,12 +58,12 @@ const CompanyPostVacancy = () => {
                                 <input type='text' name='minExp' className="rounded-xl border border-purple w-48 focus:outline-none pl-4 py-1" />
                             </div>
                         </div>
-                        <div className="flex flex-col align-start mb-3">
+                        { (jobType === 'internship') && <div className="flex flex-col align-start mb-3">
                             <label className="mb-3 font-bold text-black">Stipend:</label>
                             <div>
                                 <input type='text' name='stipend' className="rounded-xl border border-purple w-48 focus:outline-none pl-4 py-1 mb-3" />
                             </div>
-                        </div>
+                        </div> }
                         <div className="flex flex-col align-start mb-3">
                             <label className="mb-3 font-bold text-black">Skills:</label>
                             <div>
