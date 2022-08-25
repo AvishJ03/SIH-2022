9bef8c7594fe16a4fc40e9d1522442787c7e06c0
Avish Jain <avishjain0@gmail.com>
2022-08-26 05:17:17 +0530

    recmondation

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,47 +3,65 @@ import axios from "axios";
 
 const Card = ({ id }) => {
   const [jobs, setJobs] = useState({});
-  const url = "http://localhost:5000/job"
+  const [company, setCompany] = useState({});
+  const url = "http://localhost:5000"
   useEffect(() => {
     const getJob = async () => {
       try {
-        const response = await axios.get(url, {
+        const response = await axios.get(`${url}/job`, {
           id: id,
         });
-        console.log(response.data);
-        setJobs(response.data[0])
+        return response.data[0]
       } catch (error) {
         return null;
       }
     }
-    getJob()
+    const getCompany = async (id) => {
+      try {
+        const response = await axios.get(`${url}/company`, {
+          id: id,
+        });
+        return response.data[0]
+      } catch (error) {
+        return null;
+      }
+    }
+    getJob().then((job) => {
+      console.log(job);
+      setJobs(job)
+    })
+    getCompany(jobs.id).then((comp) => {
+      console.log(comp);
+      setCompany(comp)
+    })
   }, []);
   return (
     <div>
       <div className="w-11/12 h-11/12 p-5 rounded-2xl bg-white">
         <div className="flex justify-between">
           <div className="text-[#808080] text-left text-sm px-2">
-            Maximo Teams
+            {company ? company['name'] : "None"}
           </div>
           <div className="bg-black w-8 h-8 rounded-full"></div>
         </div>
         <div className="text-left font-normal text-black text-lg px-1">
-          Database Programmer
+          {jobs ? jobs['title'] : "Nan"}
         </div>
         <div className="text-left font-normal text-purple text-xs px-1">
-          Stipend
+          Stipend: {jobs ? jobs['salary'] : ""}
         </div>
         <div className="text-left font-thin text-black text-xs mx-1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur
-          imperdiet ante ut neque pretium tempus.
+          {jobs ? jobs['desc'] : "lorem ipsum"}
         </div>
         <div className="flex justify-between">
           <div className="bg-[#dfcef7] w-16 h-1/5 rounded-lg mt-5 mx-1 px-1 border border-purple">
             <div className="text-purple text-sm font-normal text-center ">
-              Remote
+              {jobs ? jobs['location'] : "Remote"}
             </div>
           </div>
-          <div className="text-sm font-normal mt-5">Location</div>
+          <div className="text-sm font-normal mt-5">
+            {jobs ? jobs['location'] : "Location"}
+          </div>
         </div>
       </div>
     </div>
--- server/routes/company.route.js
+++ server/routes/company.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Company = require('../models/company.model');
 const auth = require('../middleware/companyAuth');
+var mongoose = require('mongoose');
 
 /*
     request body
@@ -18,7 +19,7 @@ router.post('/company', async (req, res) => {
         await company.save();
         const token = await company.generateAuthToken();
         res.status(201).send({ companyUser: company, companyToken: token });
-    } catch(error) {
+    } catch (error) {
         res.status(400).send(error);
     }
 });
@@ -43,13 +44,22 @@ router.post('/company', async (req, res) => {
 router.get('/company/self', auth, async (req, res) => {
     res.status(200).send(req.companyUser);
 });
+router.get('/company', async (req, res) => {
+    try {
+        const id = mongoose.Types.ObjectId(req.body.id);
+        const comp = await Company.find({ _id: id });
+        res.status(200).send(comp);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
 
 router.post('/company/logout', auth, async (req, res) => {
     try {
         req.companyUser.tokens = [];
         await req.companyUser.save();
-        res.status(200).send({message: 'Successfully logged out.'});
-    } catch(error) {
+        res.status(200).send({ message: 'Successfully logged out.' });
+    } catch (error) {
         res.status(400).send({
             error,
             message: "Something went wrong"
@@ -71,14 +81,14 @@ router.patch('/company/self', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const validOperations = ['name', 'email', 'contactNo', 'noOfEmp', 'location', 'typeOfCompany', 'website', 'companyInfo', 'password'];
     const isUpdateValid = updates.every((update) => validOperations.includes(update));
-    if(!isUpdateValid) {
+    if (!isUpdateValid) {
         return res.status(400).send();
     }
     try {
         updates.forEach((update) => req.companyUser[update] = req.body[update]);
         await req.companyUser.save();
         res.status(200).send(req.companyUser);
-    } catch(error) {
+    } catch (error) {
         res.status(400).send({
             error: e,
             message: "Something went wrong"
@@ -90,7 +100,7 @@ router.delete('/company/self', auth, async (req, res) => {
     try {
         await req.companyUser.remove();
         res.status(200).send(req.companyUser);
-    } catch(error) {
+    } catch (error) {
         res.status(400).send({
             error: e,
             message: "Something went wrong"
@@ -102,7 +112,7 @@ router.get('/company/self/jobs', auth, async (req, res) => {
     try {
         await req.companyUser.populate('jobs');
         res.status(200).send(req.companyUser.jobs);
-    } catch(error) {
+    } catch (error) {
         res.status(400).send({
             error: e,
             message: "Something went wrong"
