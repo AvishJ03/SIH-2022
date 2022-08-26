d4c8f5444256b043d346a70e1cece6500dc0dad2
Avish Jain <avishjain0@gmail.com>
2022-08-26 12:31:38 +0530

    Student skills

--- client/src/pages/Student/Skills.js
+++ client/src/pages/Student/Skills.js
@@ -5,40 +5,85 @@ import { useNavigate } from 'react-router-dom';
 
 const Skills=() =>{
     const navigate = useNavigate();
-    const [ skillsStudent, setSkillsStudent ] = useState("");
-    const [ performScale, setPerformScale ] = useState("");
-
+    const [ softSkill, setSoftSkill ] = useState("");
+    const [ softScale, setSoftScale ] = useState("");
+    const [ hardSkill, setHardSkill ] = useState("");
+    const [ hardScale, setHardScale ] = useState("");
+    const [ skills, setSkills ] = useState([]);
+    const [counter, setCounter]=useState(0)
+    const [counterSoft, setCounterSoft]=useState(0)
 
   const url = 'http://localhost:5000';
 
-  const handleSkills = (e) => {
-    setSkillsStudent(e.target.value);
+  const addSoftSkill = (e) => {
+    e.preventDefault();
+    const data = {
+        name: softSkill,
+        level: softScale
+    }
+    const temp = [...skills, data];
+    setSkills(temp);
+    setSoftSkill("");
+    setSoftScale(0);
   }
 
-  const handleperformScale = (e) => {
-    setPerformScale(e.target.value);
+  const addHardSkill = (e) => {
+    e.preventDefault();
+    const data = {
+        name: hardSkill,
+        level: hardScale
+    }
+    const temp = [...skills, data];
+    setSkills(temp);
+    setHardSkill("");
+    setHardScale(0);
   }
+
+  const handleSoftSkills = (e) => {
+    setSoftSkill(e.target.value);
+  }
+
+  const handleHardSkills = (e) => {
+    setHardSkill(e.target.value);
+  }
+
+  const handleSoftScale = (e) => {
+    setSoftScale(e.target.value);
+  }
+
+  const handleHardScale = (e) => {
+    setHardScale(e.target.value);
+  }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        const skillStrings = skills.map((skill) => skill.name);
+        const skill = skillStrings.join(' ');
+        console.log(skills);
+        
         try {
-            const response = await axios.patch(`${url}/students/self`, {  }, {
+            const data = {
+                skills: skill, 
+                skillsStudent: skills,
+            };
+            const response = await axios.patch(`${url}/students/self`, data, {
                 headers: {
-                    'Authentication': `Bearer ${localStorage.getItem('token')}`,
-                    "Content-type": "application/json",
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                    'Content-type': "application/json",
                 }
-
             });
-
             console.log(response.data);
-            console.log(localStorage.getItem('userType'));
-
             alert("Succesful");
             navigate('/worksamples');
 
         } catch(error) {
             alert("Error");
-
+            console.log(error.message);
         }
+        setSoftSkill("");
+        setSoftScale(0);
+        setHardSkill("");
+        setHardScale(0);
     }
 
     return(
@@ -51,30 +96,43 @@ const Skills=() =>{
                 <div className="container rounded-2xl bg-purple bg-lightPurple min-w-full min-h-full p-16 shadow-main-sd">
                     <form >
                         <div className="flex flex-col min-w-full min-h-full mb-7">
-                            <label className="mb-2 font-medium">What Skills Do you have?</label>
+                            <div>Soft Skills</div>
+                            <label className="mb-2 font-medium">What Soft Skills Do you have?</label>
                             <div className="flex flex-row flex-wrap justify-between">
-                                <input type='text' name='graduation' className="rounded-xl border border-purple w-96" onChange={handleSkills}/>
+                                {/* {Array.from(Array(counterSoft)).map((c, index)=>{
+                                    return <div>
+                                        <input key={c} type="text" className='rounded-xl border border-purple w-96 m-1'></input>
+                                        <input type="range" min="1" max="5" step={1} className="accent-purple" onChange={handleperformScale} value={softScale}/>
+                                        </div>
+                                })}
+                                <button onClick={handleClickSoft}>ADD+</button> */}
+                                <input type='text' name='softskills' className="rounded-xl border border-purple w-96" onChange={handleSoftSkills} value={softSkill} />
                             </div>
+                            
                         </div>
                         <div className="flex flex-col min-w-full min-h-full mb-7">
                             <label className="mb-2 font-medium">How would you rate yourself?(on a scale of 1 to 5)</label>
                             <div className="flex flex-row flex-wrap justify-between">
-                                <input type="range" min="1" max="5" step={1} className="accent-purple" onChange={handleperformScale} value={performScale} />
+                                <input type="range" name='softscale' min="1" max="5" step={1} className="accent-purple" onChange={handleSoftScale} value={softScale} />
                             </div>
                         </div>
+                        <button onClick={addSoftSkill}>Add</button>
+
                         <div className="flex flex-col min-w-full min-h-full mb-7">
+                            <div>Hard Skills</div>
                             <label className="mb-2 font-medium">What Skills Do you have?</label>
                             <div className="flex flex-row flex-wrap justify-between">
-                                <input type='text' name='graduation' className="rounded-xl border border-purple w-96" onChange={handleperformScale} />
+                                <input type='text' name='hardskills' className="rounded-xl border border-purple w-96" onChange={handleHardSkills} value={hardSkill} />
                             </div>
                         </div>
                         
                         <div className="flex flex-col min-w-full min-h-full mb-7">
                             <label className="mb-2 font-medium">How would you rate yourself?(on a scale of 1 to 5)</label>
-                            <div className="flex flex-row flex-wrap justify-between" onChange={handleperformScale}>
-                            <input type="range" min="1" max="5" className="accent-purple"/>
+                            <div className="flex flex-row flex-wrap justify-between">
+                            <input type="range" min="1" max="5" name='hardscale' step={1} className="accent-purple" value={hardScale} onChange={handleHardScale}/>
                             </div>
                         </div>
+                        <button onClick={addHardSkill}>Add</button>
     
                         <div className="flex flex-row-reverse min-w-full min-h-full mt-[3.75rem] justify-evenly">
                         <button type="submit" className="bg-purple rounded-xl text-white p-3 px-11" onClick={handleSubmit}>Next</button>
@@ -89,4 +147,6 @@ const Skills=() =>{
       
     )
 }
+
+
 export default Skills;
